d3fdd80eba86b8df4994f0c0bb8a1225dd48211e
Tymon Jakubowski <tymon.jakubowski@gmail.com>
2019-03-16 21:18:46 +0100

    Add points in Team class

--- football/Team.cpp
+++ football/Team.cpp
@@ -5,12 +5,23 @@ namespace Football {
 
 	Team::Team(const std::string& name) : name(name)
 	{
-		this->footballers = std::vector<std::unique_ptr<Footballer>>();
+		footballers = std::vector<std::unique_ptr<Footballer>>();
+		points = 0;
 	}
 
 
 	Team::~Team()
 	{
+	}
+
+	void Team::AddPoint()
+	{
+		points++;
+	}
+
+	int Team::GetPoints() const
+	{
+		return points;
 	}
 
 	void Team::AddPlayer(std::unique_ptr<Footballer> footballer)
--- football/Team.hpp
+++ football/Team.hpp
@@ -6,10 +6,15 @@
 namespace Football {
 	class Team
 	{
+	private:
+		int points;
+
 	public:
 		Team(const std::string& name);
 		~Team();
 		void AddPlayer(std::unique_ptr<Footballer> footballer);
+		void AddPoint();
+		int GetPoints() const;
 
 		std::string name;
 		std::vector<std::unique_ptr<Footballer>> footballers;
