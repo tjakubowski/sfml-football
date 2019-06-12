c9485ae4930d2dff2c8a2065dccc5e3fd19b97f5
Tymon Jakubowski <tymon.jakubowski@gmail.com>
2019-06-12 23:18:45 +0200

    Add saving gameobject on raycast hit

--- football/FootballerRaycastCallback.cpp
+++ football/FootballerRaycastCallback.cpp
@@ -3,9 +3,19 @@
 
 namespace Football
 {
+	FootballerRaycastCallback::FootballerRaycastCallback() : gameObject(nullptr)
+	{
+	}
+
 	float32 FootballerRaycastCallback::ReportFixture(b2Fixture* fixture, const b2Vec2& point, const b2Vec2& normal, float32 fraction)
 	{
-		const auto collisionObject = static_cast<GameObject*>(fixture->GetBody()->GetUserData());
-		return collisionObject->getTag() == "ball";
+		gameObject = static_cast<GameObject*>(fixture->GetBody()->GetUserData());
+
+		return 0;
+	}
+
+	GameObject* FootballerRaycastCallback::getGameObject() const
+	{
+		return gameObject;
 	}
 }
--- football/FootballerRaycastCallback.hpp
+++ football/FootballerRaycastCallback.hpp
@@ -1,12 +1,18 @@
 #pragma once
 #include <Box2D/Dynamics/b2WorldCallbacks.h>
 #include <Box2D/Dynamics/b2Fixture.h>
+#include "GameObject.hpp"
 
 namespace Football
 {
 	class FootballerRaycastCallback : public b2RayCastCallback
 	{
+	private:
+		GameObject* gameObject;
 	public:
+		FootballerRaycastCallback();
+		GameObject* getGameObject() const;
+
 		float32 ReportFixture(b2Fixture* fixture, const b2Vec2& point, const b2Vec2& normal, float32 fraction) override;
 	};
 }
